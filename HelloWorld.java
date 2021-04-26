8769064b50907e6b003018617a0a08bf5c8acb1b
john <john.burr@pcc.edu>
2021-04-26 16:39:31 -0700

    Hello Word written

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,9 @@
 package edu.pcc.cis.hello_world;
-
+/*
+    Created by John Burr 4/26/2021
+ */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
 }
