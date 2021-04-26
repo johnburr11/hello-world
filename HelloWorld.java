3b8e879a3b880da3dd1caa38f4821b43f9c619a1
john <john.burr@pcc.edu>
2021-04-26 16:42:33 -0700

    print out date

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,13 @@
 package edu.pcc.cis.hello_world;
+
+import java.util.Date;
+
 /*
     Created by John Burr 4/26/2021
  */
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
